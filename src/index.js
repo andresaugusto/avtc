c1e947572e5e37dce6a3b82c7e3b83bd4048df25
andresaugusto <andres.augusto@icloud.com>
2021-10-20 14:36:41 -0400

    resolve attempt at deploy bugs

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   //   <App /> 
   // </Router>,
   <React.StrictMode>
-    <Router basename='/avtc'>
+    <Router basename='/avtc/'>
       <App />
     </Router>
   </React.StrictMode>,
